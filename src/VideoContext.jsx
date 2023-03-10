1c58565a1e6e92c51236c63ba2440ea08d1af9bc
Ujjawal_Tyagi <tyagiujjawal9@gmail.com>
2023-03-10 15:25:26 +0530

    first commit

---
+++ src/VideoContext.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, createContext } from "react";
+import axios from "axios";
+
+export const VideoContext = createContext();
+export const VideoProvider = (props) => {
+  const [apiVideos, setApiVideos] = useState([]);
+
+  const key = process.env.REACT_APP_API_KEY;
+  const maxResults = 30;
+  useEffect(() => {
+    console.log(key);
+    axios
+      .get(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2C%20statistics%2C%20contentDetails&chart=mostPopular&maxResults=${maxResults}&key=${key}`
+      )
+      .then((response) => {
+        const { data } = response;
+        const videoResults = data["items"];
+        setApiVideos(videoResults);
+        console.log(videoResults);
+      });
+  }, []);
+  return (
+    <VideoContext.Provider value={[apiVideos, setApiVideos]}>
+      {props.children}
+    </VideoContext.Provider>
+  );
+};
